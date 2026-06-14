7f9170f694b9050b718a99c97d3da0b5c99d7d0f
PKSE Deploy <pkse@pkseugm.web.id>
2026-06-14 18:17:12 +0700

    fix: kill rapor login page, redirect to /login, back link to /member

--- admin-ui/src/rapor.tsx
+++ admin-ui/src/rapor.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import MemberRaporPage from './pages/MemberRaporPage'
 import EntryDetailPage from './pages/EntryDetailPage'
 
@@ -9,7 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter basename="/rapor">
       <Routes>
-        <Route index element={<LoginPage />} />
+        <Route index element={<Navigate to="/login" replace />} />
         <Route path="m/:id" element={<MemberRaporPage />} />
         <Route path="t/:token" element={<EntryDetailPage />} />
       </Routes>
